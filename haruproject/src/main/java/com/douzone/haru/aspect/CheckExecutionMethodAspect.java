24e2db0fa173bf3ec88c4c5371b969c3c2098ba1
stpn94 <stpn94@gmail.com>
2021-11-29 18:33:48 +0900

    #이종윤 [SET]: (Haru) 처음 Setting

---
+++ haruproject/src/main/java/com/douzone/haru/aspect/CheckExecutionMethodAspect.java
@@ -0,0 +1,24 @@
+package com.douzone.haru.aspect;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@EnableAspectJAutoProxy
+@Component
+public class CheckExecutionMethodAspect {
+	
+	@Around("execution(* *..*.repository.*.*(..)) || execution(* *..*.service.*.*(..)) || execution(* *..*.controller.*.*(..))")
+	public Object aroundAdvice(ProceedingJoinPoint pjp) throws Throwable {
+		Object result = pjp.proceed();
+		
+		// after
+		String className = pjp.getTarget().getClass().getName();
+		String methodName = pjp.getSignature().getName();
+		String taskName = className + "." + methodName;
+		
+		System.out.println("[Execution][" + taskName + "]");
+		
+		return result;
+	}
+}
