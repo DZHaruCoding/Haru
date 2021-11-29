6c202880ed179c8b533472a02a1ba481774c719f
stpn94 <stpn94@gmail.com>
2021-11-29 18:41:49 +0900

    #이종윤 [ADD] [Haru] simpleJSON 추가

---
+++ haruproject/src/main/java/com/douzone/haru/service/HistoryService.java
@@ -0,0 +1,107 @@
+package com.douzone.haru.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.douzone.haru.repository.HistoryRepository;
+import com.douzone.haru.vo.HistoryVo;
+
+@Service
+public class HistoryService {
+
+	@Autowired
+	private HistoryRepository historyRepository;
+	
+	public List<HistoryVo> selectHistory(Long projectNo) {
+		return historyRepository.selectHistory(projectNo);
+	}
+
+	/*
+	 * 작성자:김우경
+	 * 설명:히스토리추가 
+	 */
+	public boolean insertHistory(JSONObject historyJson) {
+		Map<String, Object> map = new HashMap<>();
+		
+		HashMap memberInfo = new HashMap<>();
+		
+		map.put("logDate", historyJson.get("historyDate"));
+		map.put("projectNo", historyJson.get("projectNo"));
+		System.out.println(historyJson.get("actionName"));
+		if(historyJson.get("actionName") instanceof String == false) {
+			memberInfo = (HashMap) historyJson.get("actionName");			
+			System.err.println(memberInfo);
+		}
+		
+		
+		switch((String) historyJson.get("historyType")) {
+			case "taskContentsUpdate":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 으로 업무이름을 수정하셨습니다.");
+				break;
+			case "taskListInsert":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무리스트를 추가하였습니다.");
+				break;
+			case "taskListDelete":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무리스트를 삭제하였습니다.");
+				break;
+			case "taskDateUpdate":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무의 마감일을 수정하였습니다.");
+				break;
+			case "taskMemberJoin":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무에 멤버를 추가하였습니다.");
+				break;
+			case "checklistInsert":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무에 체크리스트를 추가하였습니다.");
+				break;
+			case "checklistStateUpdate":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무의 체크리스트 상태를 수정하였습니다.");
+				break;
+			case "commentInsert":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무에 코멘트를 추가하였습니다.");
+				break;
+			case "taskDragNdrop":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무의 위치를 변경하였습니다.");
+				break;
+			case "taskListDragNdrop":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무리스트의 위치를 변경하였습니다.");
+				break;
+			case "taskStateUpdate":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무 상태를 변경하였습니다.");
+				break;
+			case "taskInsert":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무를 추가하였습니다.");
+				break;
+			case "taskDelete":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 업무를 삭제하였습니다.");
+				break;
+			case "projectMemberInvite":
+				if(memberInfo.get("memberName") == "") {
+					map.put("logContents", historyJson.get("senderName") +" 님이 " + memberInfo.get("memberEmail") + " 님을 초대하였습니다.");
+				} else {
+					map.put("logContents", historyJson.get("senderName") +" 님이 " + memberInfo.get("memberName") + " 님을 초대하였습니다.");
+				}
+				break;
+			case "projectMemberJoin":
+				System.err.println(historyJson.get("actionName"));
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 님을 프로젝트에 참여시켰습니다.");				
+				break;
+			case "projectDateUpdate":
+				map.put("logContents", historyJson.get("senderName") +" 님이 " + historyJson.get("actionName") + " 프로젝트의 업무마감일을 수정하였습니다.");
+				break;
+		}
+		
+		int result = historyRepository.insertHistory(map);
+		return result == 1;
+	}
+
+	private char[] Instanceof(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
