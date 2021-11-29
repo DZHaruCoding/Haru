493a9cf73acc91d5ce4e5dfcf3c5f1cc7cc35644
stpn94 <stpn94@gmail.com>
2021-11-29 23:22:18 +0900

    #종윤 [DOC] (query) DML 작성중

--- documents/workbench/test종윤.sql
+++ documents/workbench/test종윤.sql
@@ -1,1 +1,17 @@
 -- 종윤
+
+-- checklist
+
+SELECT task_no, checklist_no, checklist_contents, checklist_state
+  FROM checklist;
+
+INSERT 
+  INTO checklist (task_no, checklist_no, checklist_contents)
+VALUES (task_no, checklist_no, checklist_contents);
+
+UPDATE checklist	
+   SET task_no = task_no,
+	   checklist_no = checklist_no,
+	   checklist_contents = checklist_contents,
+	   checklist_state = do
+ WHERE checklist_no = checklist_no;
