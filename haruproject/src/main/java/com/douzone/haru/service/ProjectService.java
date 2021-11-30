645116d0cbf1813b54cb027ceef8a452d9928641
kimhaengun <chxl89@naver.com>
2021-11-30 22:56:03 +0900

    #김행운[add]: 프로젝트생성(유저x),로그인된유저 프로젝트 추가 완료

--- haruproject/src/main/java/com/douzone/haru/service/ProjectService.java
+++ haruproject/src/main/java/com/douzone/haru/service/ProjectService.java
@@ -1,5 +1,8 @@
 package com.douzone.haru.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +19,24 @@ public class ProjectService {
 	public void ProjectInsert() {
 		// TODO Auto-generated method stub
 		ProjectVo projectVo = new ProjectVo();
-		projectVo.setProjectTitle("settest");
-		projectVo.setProjectDesc("settest");
+		projectVo.setProjectTitle("settest4");
+		projectVo.setProjectDesc("settest4");
 		projectVo.setProjectStart("2021-12-01 00:00:00");
 		projectVo.setProjectEnd("2021-12-31 01:00:00");
 		projectVo.setProjectState("T");
 		projectVo.setProjectRegDate("2021-11-30 20:42:00");
 		System.out.println("======="+projectVo+"==========");
-		projectRepository.procjectInsert(projectVo);
+//		1.프로젝트 생성 완료
+		//selectKey를 사용하면 return값은 1이고 vo에 project no값이 자동으로 들어감
+		int projectNo = projectRepository.procjectInsert(projectVo);
+//		2.로그인된 유저 생성된 프로젝트 삽입
+		Map<String, Object> map = new HashMap<String, Object>();
+		projectVo.setUserNo(1L);
+		map.put("userNo",projectVo.getUserNo());
+		map.put("projectNo", projectVo.getProjectNo());
+		
+		projectRepository.userProjectInsert(map);
+		
 	}
 	
 }
