282ca367c9c192a69df528cdd5e4ca5dbb4bd8d1
kimhaengun <chxl89@naver.com>
2021-11-30 17:56:29 +0900

    #김행운[add]:파일 업로드, 프로젝트 생성(~ing)

--- haruproject/src/main/java/com/douzone/haru/config/WebConfig.java
+++ haruproject/src/main/java/com/douzone/haru/config/WebConfig.java
@@ -4,15 +4,24 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@PropertySource({"classpath:com/douzone/haru/config/WebConfig.properties"})
 public class WebConfig implements WebMvcConfigurer {
+	//FileUpload
+	@Autowired
+	private Environment env;
+	
 	// Message Converters
 		@Bean
 		public StringHttpMessageConverter stringHttpMessageConverter() {
@@ -28,5 +37,14 @@ public class WebConfig implements WebMvcConfigurer {
 		@Override
 		public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 			converters.add(stringHttpMessageConverter());
+		}
+		
+		//FileUpload (ResourceMapping (URL Magic Mapping))
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			// TODO Auto-generated method stub
+			registry
+			.addResourceHandler(env.getProperty("fileupload.resourceMapping"))
+			.addResourceLocations("file:"+env.getProperty("fileupload.uploadLocation"));
 		}
 }
