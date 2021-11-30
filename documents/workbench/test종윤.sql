b7497375376bd08b2167e190ff4f786fcde32381
stpn94 <stpn94@gmail.com>
2021-11-30 21:42:42 +0900

    #종윤 [ADD](file.xml) file query작성

--- documents/workbench/test종윤.sql
+++ documents/workbench/test종윤.sql
@@ -45,4 +45,64 @@ select log_no as logNo,	log_date as logDate, log_contents as logContents, projec
  order by log_no desc;
  
 -- insertHistory
--- insert into history values(#{projectNo}, null, now(), #{logContents})
+-- insert into history (project_no, log_contents)
+-- values( #{projectNo},#{logContents});
+insert into history (project_no, log_contents)
+values( 1, 'log_contents');
+
+-- findByFileNo
+-- select change_name 
+--   from file
+--  where file_no = #{fileNo}
+select change_name 
+  from file
+ where file_no = 1;
+
+-- insertFile
+-- insert
+--   into file (task_no, file_no, origin_name, change_name, file_path, file_regdate, file_state, file_maker)
+-- values (#{taskNo}, #{originName}, #{changeName}, #{filePath}, #{fileMaker})
+insert
+  into file (task_no, origin_name, change_name, file_path, file_maker)
+values (1, '12', '23','34','file_maker');
+
+-- selectFile
+-- select distinct
+-- 		p.project_no as projectNo,
+--      p.project_title as projectTitle,
+--      tl.tasklist_no as tasklistNo,
+--      tl.tasklist_name as tasklistName,
+-- 		t.task_no as taskNo,
+-- 		t.task_contents as taskContents,
+-- 		t.task_state as taskState,
+-- 		f.file_no as fileNo,
+--      f.file_path as filePath, 
+--      f.origin_name as originName,
+--      f.file_regdate as fileRegdate,
+--      f.file_state as fileState,
+--      f.file_maker as fileUploader
+-- from project p
+-- join tasklist tl on (p.project_no = tl.project_no)
+-- join task t on (tl.tasklist_no = t.tasklist_no)
+-- join file f on(t.task_no = f.task_no)
+-- where p.project_no=#{projectNo} and t.task_state!='del' and f.file_state = 'T';
+
+select distinct
+		p.project_no as projectNo,
+        p.project_title as projectTitle,
+        tl.tasklist_no as tasklistNo,
+        tl.tasklist_name as tasklistName,
+		t.task_no as taskNo,
+		t.task_contents as taskContents,
+		t.task_state as taskState,
+		f.file_no as fileNo,
+        f.file_path as filePath, 
+        f.origin_name as originName,
+        f.file_regdate as fileRegdate,
+        f.file_state as fileState,
+        f.file_maker as fileUploader
+from project p
+join tasklist tl on (p.project_no = tl.project_no)
+join task t on (tl.tasklist_no = t.tasklist_no)
+join file f on(t.task_no = f.task_no)
+where p.project_no=1 and t.task_state!='del' and f.file_state = 'T';
