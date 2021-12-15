a634b3557578a65e1d63a026d00ae2daf1a8d091
DESKTOP-ITCR4H8\wlstj <DESKTOP-ITCR4H8\wlstj>
2021-12-15 22:25:30 +0900

    #조진석 [MODIFY] (kanban socket) 칸반 리스트 소켓 수정

--- haruproject/src/main/java/com/douzone/haru/controller/api/ApiTasklistController.java
+++ haruproject/src/main/java/com/douzone/haru/controller/api/ApiTasklistController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.douzone.haru.config.auth.PrincipalDetails;
+import com.douzone.haru.config.auth.scurity.AuthUser;
 import com.douzone.haru.dto.JsonResult;
 import com.douzone.haru.service.NoticeMessageService;
 import com.douzone.haru.service.ProjectService;
@@ -84,7 +85,7 @@ public class ApiTasklistController {
 	
 //	@AuthenticationPrincipal PrincipalDetails userVo,
 	@PostMapping("/add")
-	public JsonResult insertTaskList(@RequestBody Map<String, Object> vo) {
+	public JsonResult insertTaskList(@RequestBody Map<String, Object> vo, @AuthUser PrincipalDetails principa) {
 		
 //		System.out.println(user.getUserNo());
 		
@@ -92,6 +93,8 @@ public class ApiTasklistController {
 		tlVo.setProjectNo((Integer)vo.get("projectNo"));
 		tlVo.setTaskListName((String)vo.get("taskListName"));
 		tlVo.setTaskListOrder((Integer)vo.get("taskListOrder"));
+		
+		System.out.println("asdsada"+principa.getUserNo());
 		
 		String userEmail = (String)vo.get("userEmail");
 		
@@ -102,6 +105,14 @@ public class ApiTasklistController {
 			map.put("projectNo", tlVo.getProjectNo());
 			map.put("userEmail", userEmail);
 			
+			//맴버 있는지 체크
+			List<UserVo> member = projectService.proejctmemberAlllistselect(tlVo.getProjectNo());
+			
+			if (member.size() == 0) {
+				return JsonResult.success(result);
+			}
+			
+			
 			// 알림 추가
 			String message = ((String) vo.get("projectName")) + "에 테스크 리스트가 추가되었습니다.";
 			NoticeMessageVo messageVo = new NoticeMessageVo();
@@ -109,10 +120,8 @@ public class ApiTasklistController {
 			messageVo = noticeMessageService.noticeInsert(messageVo);
 			
 			// 맴버들에게 알림
-			List<UserVo> member = projectService.proejctmemberAlllistselect(tlVo.getProjectNo());
-			
 			for (UserVo userVo : member) {
-				if (!userVo.getUserEmail().equals(userEmail)) {
+				if (userVo.getUserNo() != (principa.getUserNo())) {
 					MessageBoxVo mbVo = new MessageBoxVo();
 					mbVo.setUserNo(userVo.getUserNo());
 					mbVo.setNoticeNo(messageVo.getNoticeNo());
@@ -121,11 +130,14 @@ public class ApiTasklistController {
 				}
 			}
 			
+			// 소켓
 			Map<String, Object> resultSend = new HashMap<String, Object>();
 			resultSend.put("data", vo);
+			resultSend.put("bellNo",messageVo.getNoticeNo());
 			resultSend.put("bell", message);
+			resultSend.put("taskListNo", result);
 			
-			apiNoticeSocket.taskUpdateSend(resultSend, member, userEmail);
+			apiNoticeSocket.taskUpdateSend(resultSend, member, principa.getUserNo());
 			
 			return JsonResult.success(result);
 		} else {
@@ -136,12 +148,45 @@ public class ApiTasklistController {
 	
 	@PostMapping("/delete")
 	@Transactional
-	public JsonResult deleteTaskList(@RequestBody long no) {
+	public JsonResult deleteTaskList(@RequestBody Map<String, Object> map, @AuthUser PrincipalDetails principa) {
+		System.out.println(principa.getUserNo());
 		
-		long result1 = tasklistService.deleteTaskList(no);
-		long result2 = taskService.deleteByTaskList(no);
+		long result1 = tasklistService.deleteTaskList((Integer)map.get("taskListNo"));
+		long result2 = taskService.deleteByTaskList((Integer)map.get("taskListNo"));
 		
 		if (result1 > 0) {
+			
+			List<UserVo> member = projectService.proejctmemberAlllistselect((Integer)map.get("projectNo"));
+			
+			if (member.size() == 0) {
+				return JsonResult.success(result1);
+			}
+			
+			// 알림 추가
+			String message = ((String) map.get("projectName")) + "에 테스크 리스트가 삭제되었습니다.";
+			NoticeMessageVo messageVo = new NoticeMessageVo();
+			messageVo.setNoticeMessage(message);
+			messageVo = noticeMessageService.noticeInsert(messageVo);
+			
+			// 맴버들에게 알림
+			for (UserVo userVo : member) {
+				if (userVo.getUserNo() != (principa.getUserNo())) {
+					MessageBoxVo mbVo = new MessageBoxVo();
+					mbVo.setUserNo(userVo.getUserNo());
+					mbVo.setNoticeNo(messageVo.getNoticeNo());
+								
+					noticeMessageService.noticeBoxInsert(mbVo);
+				}
+			}
+			
+			// 소켓
+			Map<String, Object> resultSend = new HashMap<String, Object>();
+			resultSend.put("data", (Integer)map.get("taskListNo"));
+			resultSend.put("bellNo",messageVo.getNoticeNo());
+			resultSend.put("bell", message);
+						
+			apiNoticeSocket.taskRemoveSend(resultSend, member, principa.getUserNo());
+			
 			return JsonResult.success(result1);
 		} else {
 			return JsonResult.fail("삭제 실패");
